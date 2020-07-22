344cc57427d5029d058d54f6549308e365dbdd65
Dominik Bieczynski <dominik.bieczynski.ext@nokia.com>
2020-07-22 14:03:12 +0200

    Add command line script

--- Test1.py
+++ Test1.py
@@ -1,3 +1,29 @@
-name = input("Podaj imię: ")
-print("Hello " + name)
-print("Nowa linia " + name)
+#!/usr/bin/env python
+# coding: utf8
+
+def SayHello():
+    name = input(u"Podaj imię: ")
+    print("Hello " + name)
+    print("Nowa linia " + name)
+
+def main():
+    print("Witamy w funkcji main")
+    while True:
+        cmd = input("> ")
+        
+        if cmd == "exit":
+            break
+            
+        elif cmd == "name":
+            SayHello()
+            
+        elif cmd == "test":
+            print("Hello")
+
+if __name__ == "__main__":
+	main()
+    
+def test():
+    print("Witamy w funkcji test")
+    
+print("Koniec pliku")
