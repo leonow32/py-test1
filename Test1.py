156a5505ba1630332494eb1d5ba65b47fd8de7ad
Dominik Bieczynski <leonow32@gmail.com>
2020-08-10 20:55:17 +0200

    DictView - dodano wciecia oraz lambdy

--- Test1.py
+++ Test1.py
@@ -1,5 +1,12 @@
 # Test slownikow
 
+def Funkcja1(x):
+    return x+1
+
+def Funkcja2(x):
+    return x+1
+
+
 D = {
     "a": "aaa",
     "b": "bbb",
@@ -12,18 +19,37 @@ D = {
             "i": "iii",
             "j": "jjj",
             },
-        "k": "kkk",
+        "k": {
+            "l": "lll",
+            "m": "mmm",
+            },
         },
-    "l": "lll",
+    "n": "nnn",
+    "o": Funkcja1,
+    "p": Funkcja1,
+    "q": Funkcja2,
+    "r": Funkcja2,
+    "s": lambda x: Funkcja1(x),
+    "t": lambda: Funkcja1(1),
     }
 
 
 def printDict(DictInstance, NestLevel):
     for a, b in DictInstance.items():
-        print("x" * NestLevel, a, ": ", b)
+        if type(b) == dict:
+            print("    " * NestLevel, a, ": ")
+            printDict(b, NestLevel+1)
+        else:
+            print("    " * NestLevel, a, ": ", b, type(b))
 
     return
 
-print(D)
-
-printDict(D, 2)
+printDict(D, 0)
+print("==1==")
+arg = input("wpisz s:")
+#print(D["s"](123))
+print(D[arg](123))
+print("==2==")
+arg = input("wpisz t:")
+#print(D["t"]())
+print(D[arg]())
