b0a357ce66d8d62fa13931693872b29266881602
Dominik Bieczynski <leonow32@gmail.com>
2020-08-11 22:46:04 +0200

    Lambda Interpreter - prosty interpreter polecen przy pomocy slownika i funkcji lambda

--- lambda.py
+++ lambda.py
@@ -1,21 +1,49 @@
-# Test slowniko i funkcji lambda
+# Test slownika i funkcji lambda
 
-def OperationNone(x):
-    return "Default"
-
-def OperationInc(x):
-    return x+1
-
-def OperationDec(x):
-    return x-1
-
-
+# Tablica wszystkich poleceń
 Commands = {
-    "inc": lambda arg: OperationInc(arg),
-    "dec": lambda arg: OperationDec(arg),
+    "inc":              lambda Args: OperationInc(Args),
+    "dec":              lambda Args: OperationDec(Args),
+    "+":                lambda Args: OperationAdd(Args),
+    "-":                lambda Args: OperationSub(Args),
+    "*":                lambda Args: OperationMul(Args),
+    "/":                lambda Args: OperationDiv(Args),
+    "show":             lambda Args: ShowArgs(Args),
+    "pd":               lambda Args: PrintDict1(Args),
+    "exit":             lambda Args: Exit(Args),
     }
 
-Commands.setdefault("0", OperationNone)
+def Exit(Args):
+    quit
+
+def ShowArgs(Args):
+    i = 0;
+    for value in Args:
+        print("{}\t{}".format(i, value))
+        i = i+1
+    return ""
+
+def OperationInc(Args):
+    return float(Args[1]) + 1
+
+def OperationDec(Args):
+    return float(Args[1]) - 1
+
+def OperationAdd(Args):
+    return float(Args[1]) + float(Args[2])
+
+def OperationSub(Args):
+    return float(Args[1]) - float(Args[2])
+
+def OperationMul(Args):
+    return float(Args[1]) * float(Args[2])
+
+def OperationDiv(Args):
+    return float(Args[1]) / float(Args[2])
+
+def PrintDictCmd(Args):
+    PrintDict(Commands, 0)
+    return ""
 
 def PrintDict(DictInstance, NestLevel):
     for a, b in DictInstance.items():
@@ -25,28 +53,14 @@ def PrintDict(DictInstance, NestLevel):
         else:
             print("    " * NestLevel, a, ":", b, type(b))
 
-    return
-
-cmd = ""
-
-
-
 def main():
     PrintDict(Commands, 0)
+    
     while True:
-        cmd = input("> ")
-        
-        if cmd == "exit":
-            quit()
-            
-        elif cmd == "print":
-            PrintDict(Commands, 0)
-            
-        elif cmd == "test":
-            print("Hello")
-
-        else:
-            print(Commands[cmd](100))
+        Args = input("> ")                                                          # Prompt
+        Args = Args.split()                                                         # Rozbijanie wiersza polecen na poszczegolne wyrazy
+        if len(Args) == 0: continue                                                 # Kontrola czy wprowadzono chociaz jeden argument
+        print(Commands[Args[0]](Args) if Args[0] in Commands else "Bad command")    # Wykonanie polecenia
 
 if __name__ == "__main__":
 	main()
